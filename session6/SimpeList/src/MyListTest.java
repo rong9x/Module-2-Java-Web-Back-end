2aa01fde89f9ba30000868c59618193867a4a91f
rong9x <49978600+rong9x@users.noreply.github.com>
2019-06-10 08:37:35 +0700

    ss6

---
+++ session6/SimpeList/src/MyListTest.java
@@ -0,0 +1,19 @@
+public class MyListTest {
+    public static void main(String[] args) {
+        MyList<Integer> listInteger = new MyList<Integer>();
+        listInteger.add(1);
+        listInteger.add(2);
+        listInteger.add(3);
+        listInteger.add(3);
+        listInteger.add(4);
+
+        System.out.println(listInteger.get(2));
+        System.out.println(listInteger.get(3));
+
+        //listInteger.get(6);
+        //System.out.println(listInteger.get(6));
+
+        listInteger.get(-1);
+        System.out.println("element -1: " + listInteger.get(-1));
+    }
+}
