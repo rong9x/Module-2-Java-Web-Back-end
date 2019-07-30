2d3393c0a14ecafdc5b1919ada1137c5744462d9
rong9x <49978600+rong9x@users.noreply.github.com>
2019-07-30 14:26:21 +0700

    customer-management

---
+++ session19/customer-management/src/main/java/com/codegym/cms/model/Province.java
@@ -0,0 +1,47 @@
+package com.codegym.cms.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "provinces")
+public class Province {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String provinceName;
+
+    @OneToMany(targetEntity = Customer.class)
+    private List<Customer> customers;
+
+    public Province(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public Province() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public List<Customer> getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(List<Customer> customers) {
+        this.customers = customers;
+    }
+}
